4b761698e4fcd1c8c54b9514c050d885da46f9df
Ritik Kumar <70974819+Ritik6118@users.noreply.github.com>
2025-11-11 19:52:38 +0530

    Greedy and 2 pointer

---
+++ Lec_09/Get_The_Maximum_Score.java
@@ -0,0 +1,49 @@
+package Lec_09;
+
+public class Get_The_Maximum_Score {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] arr1= {2,4,5,8,10};
+		int [] arr2= {4,6,8,9};
+		maxSum(arr1,arr2);
+	}
+
+	public static int maxSum(int[] nums1, int[] nums2) {
+		int i=0;   // pointer for nums 1
+		int j=0;   // pointer for nums 2
+		long si=0;	// sum by path of pointer i
+		long sj=0;	// sum by path of pointer j
+		long sum=0;  // global sum
+		
+		while(i<nums1.length && j<nums2.length) {
+			if(nums1[i]<nums2[j]) {
+				si+=nums1[i];
+				i++;
+			}
+			else if(nums2[j]<nums1[i]) {
+				sj+=nums2[j];
+				j++;
+			}
+			else {
+				sum+=Math.max(si, sj)+nums1[i];
+				i++;
+				j++;
+				si=0;
+				sj=0;
+			}
+		}
+		while(i<nums1.length) {
+			si+=nums1[i];
+			i++;			
+		}
+		while(j<nums2.length) {
+			sj+=nums2[j];
+			j++;			
+		}
+		sum+=Math.max(si, sj);
+		sum%=1000000007;
+		return (int)sum;
+	}
+
+}
